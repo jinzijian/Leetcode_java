ccc521fdd6e5a182a510bb944abdc339871f03f3
Jin zijian <1034158844@qq.com>
2020-01-25 16:37:37 +0800

    1

--- lists/92 反转链表|| 迭代法/solution.java
+++ lists/92 反转链表|| 迭代法/solution.java
@@ -1,38 +1,31 @@
-/**迭代法*/
- /**先部分反转再重新连接*/
+/**头插法*/
+ /**举例：1-2-3-4-5变为1-3-4-2-5*/
 class Solution {
     public ListNode reverseBetween(ListNode head, int m, int n) {
-        ListNode tmp = null;
-        ListNode top = null;/**用来记录m前的一个节点*/
-        ListNode tail = null;/**用来记录m节点*/
-        ListNode pre = null;
-        ListNode cur = head;
-        /**这里m>1下面n>0 是因为cur要越过n*/
-        while(m>1){
-            m--;
-            n--;
-            pre = cur;
-            cur = cur.next;
-        }
-        top = pre;
-        tail = cur;
-        /**反转链表结束 cur指向n的后一个*/
-        while(n>0){
-            n--;
-            tmp = cur.next;
-            cur.next = pre;
-            pre = cur;
-            cur = tmp;
-            /**cur在这里指向n后面的一个节点*/
-        }
-        /**要判断top是否存在避免从第一个节点开始反转的情况*/
-        if (top!=null){
-            top.next = pre;
-        }else{
-            head = pre;
-        }
-        tail.next = cur;
-        /**重新连接起来*/
-        return head;
+       ListNode cur = head;
+       ListNode dummy = new ListNode(0);
+       dummy.next = head;
+       ListNode tmp = null; 
+       ListNode pre = dummy;
+       /**pre指向1，cur指向2*/
+       while (m>1){
+           m--;
+           n--;
+           pre = cur;
+           cur = cur.next;
+       }
+      while(n>1){
+          n--;
+          tmp = cur.next;
+          /**tmp记录3-4-5*/
+          cur.next  = tmp.next;
+          /**2-4-5，把3断开*/
+          tmp.next = pre.next;
+          /**把3-2连上变为3-2-4-5*/
+          pre.next = tmp;  
+          /**把1和3连上变为1-3-2-4-5*/
+          /**现在cur还是指向2.pre还是指向1*/
+      }
+      return dummy.next;
     }
 }
