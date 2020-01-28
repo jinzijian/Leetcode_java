7ea3b9f2177fe007305a3a78b6685f4a5c806601
Jin zijian <1034158844@qq.com>
2020-01-28 22:53:30 +0800

    fix

--- README.md
+++ README.md
@@ -2,4 +2,11 @@
 
 配合cs61b和普林斯顿算法食用更香哦~
 
-- 最基本的数据结构：[链表](https://github.com/jinzijian/leetcode_java/tree/master/lists)
+## 第一节：[链表](https://github.com/jinzijian/leetcode_java/tree/master/lists)
+
+链表是最基础的数据结构，具有插入删除等无需移动元素且无需事先估计存储空间的特点，但缺陷是无法随机访问任意元素，只能通过遍历的方法来访问。
+
+链表题的基础在于对节点的保存和重定向，常用的技巧为虚拟头结点和双指针方法。
+
+下面我们就从一道双节点问题开始。
+
---
+++ lists/876  链表中点 快慢指针法/solution.java
@@ -0,0 +1,19 @@
+/**快慢指针法:可以无视奇偶，但记得是指向奇数中点和偶数靠右侧的中点。*/
+ /**若链表长度为偶数时需要靠左的中点则 终止条件为：fast.next!=null && fast.next.next!= null*/
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head.next==null){
+            return head;
+        }
+        if(head.next.next == null){
+            return head.next;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null && fast.next!= null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
