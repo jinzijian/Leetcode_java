730ac77799e8f49afd6561dfe68a87363cfb6541
Jin zijian <1034158844@qq.com>
2020-01-31 12:35:18 +0800

    Create solution.java

---
+++ 01 LinkedLists/061 旋转链表 每次旋转一位/solution.java
@@ -0,0 +1,39 @@
+/**每次旋转一位*/
+class Solution {
+    public int findsize(ListNode head){
+        ListNode tmp = head;
+        int size = 0;
+        while(tmp!= null){
+            tmp = tmp.next;
+            size+=1;
+        }
+        return size;
+    }
+    public ListNode rotateone(ListNode head){
+        if(head == null || head.next == null){
+            return head;
+        }
+        ListNode f = head.next;
+        ListNode s = head;
+        while(f.next!=null){
+            f = f.next;
+            s = s.next;
+        }
+        s.next = null;
+        f.next = head;
+        return f;
+
+    }
+    public ListNode rotateRight(ListNode head, int k) {
+        if(head == null || head.next == null){
+            return head;
+        }
+        int size = findsize(head);
+        k = k%size;
+        while(k>0){
+            head = rotateone(head);
+            k -= 1;
+        }
+        return head;
+    }
+}
